fd13beafa3ccd92030f4bfa9f1eef09c3df46405
wqh0109663 <337096961@qq.com>
2018-12-24 20:23:34 +0800

    xml 解析未完成

--- src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/MultiRootXmlParserDemo.java
+++ src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/MultiRootXmlParserDemo.java
@@ -1,11 +1,18 @@
 package top.shownmmp.spring.enhance.project.java.base.knowledge.xml;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Iterator;
 
 /**
+ * 从数据库中解析出包含xml的字段信息，所有 TODO
+ *
  * @author 吴启欢
  * @version 1.0
  * @date 2018-12-22 11:37
@@ -21,15 +28,32 @@ public class MultiRootXmlParserDemo {
 
         Class.forName(DRIVER);
         conn = DriverManager.getConnection(URL, USER, PASSWORD);
-        String sql = "select extend_data_xml from km_review_main where " +
-                " fd_template_id='150461343fce1f2e6c14a7d4eb2808bd'   ";
+        String sql = "select extend_data_xml from szgt_fterp_main where fd_id='1600ab2e240e400b26c0f8e4c66b803a'";
         PreparedStatement statement = conn.prepareStatement(sql);
         ResultSet resultSet = statement.executeQuery();
-        resultSet.next();
+        String str ="";
         while (resultSet.next()) {
-            String str = resultSet.getString(1);
-        }
+            str = resultSet.getString(1);
+            System.out.println(str);
 
+        }
+        // 将字符串转为XML
+        Document doc = DocumentHelper.parseText(str);
+        Element rootElt = doc.getRootElement();
+
+
+
+        System.out.println("根节点：" + rootElt.getName());
+        Iterator iter = rootElt.elementIterator("object");
+        while (iter.hasNext()) {
+            Element recordEle = (Element) iter.next();
+            Iterator iters = recordEle.elementIterator("void");
+            while (iters.hasNext()) {
+                Element itemEle = (Element) iters.next();
+                System.out.println(itemEle);
+
+            }
+        }
 
         resultSet.close();
         statement.close();
---
+++ src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/XmlParser.java
@@ -0,0 +1,66 @@
+package top.shownmmp.spring.enhance.project.java.base.knowledge.xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+/**
+ * @author 吴启欢
+ * @version 1.0
+ * @date 2018-12-24 17:21
+ */
+public class XmlParser {
+    public static void main(String[] args) throws Exception {
+        try {
+            File inputFile = new File("C:\\Users\\Administrator\\Desktop\\aaa.txt");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            System.out.println();
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("student");
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Student roll no : "
+                            + eElement.getAttribute("rollno"));
+                    System.out.println("First Name : "
+                            + eElement
+                            .getElementsByTagName("firstname")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Last Name : "
+                            + eElement
+                            .getElementsByTagName("lastname")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Nick Name : "
+                            + eElement
+                            .getElementsByTagName("nickname")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Marks : "
+                            + eElement
+                            .getElementsByTagName("marks")
+                            .item(0)
+                            .getTextContent());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
