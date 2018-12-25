4a8c5ef67421cf187243e0fee276652523697b08
wqh0109663 <337096961@qq.com>
2018-12-25 10:52:03 +0800

    xml parser

--- src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/MultiRootXmlParserDemo.java
+++ src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/MultiRootXmlParserDemo.java
@@ -9,9 +9,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Iterator;
+import java.util.List;
 
 /**
- * 从数据库中解析出包含xml的字段信息，所有 TODO
+ * 从数据库中解析出包含xml的字段信息
  *
  * @author 吴启欢
  * @version 1.0
@@ -45,13 +46,20 @@ public class MultiRootXmlParserDemo {
 
         System.out.println("根节点：" + rootElt.getName());
         Iterator iter = rootElt.elementIterator("object");
+        String fd = "";
+        flag:
         while (iter.hasNext()) {
             Element recordEle = (Element) iter.next();
-            Iterator iters = recordEle.elementIterator("void");
-            while (iters.hasNext()) {
-                Element itemEle = (Element) iters.next();
-                System.out.println(itemEle);
-
+            List<Element> list = recordEle.elements("void");
+            for (Element element:list){
+                List<Element> string = element.elements("string");
+                for (int i = 0;i<string.size();i++){
+                    if ("fd_location".equals(string.get(i).getTextTrim())){
+                        fd = string.get(i+1).getTextTrim();
+                        System.out.println("fd::"+fd);
+                        break flag;
+                    }
+                }
             }
         }
 
