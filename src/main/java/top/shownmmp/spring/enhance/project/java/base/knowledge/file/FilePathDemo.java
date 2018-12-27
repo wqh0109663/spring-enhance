1cafb1d1adcb4e81862c0a1ce82ea5d2fd21aa90
wqh0109663 <337096961@qq.com>
2018-12-27 21:04:14 +0800

    file的使用

---
+++ src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/file/FilePathDemo.java
@@ -0,0 +1,16 @@
+package top.shownmmp.spring.enhance.project.java.base.knowledge.file;
+
+/**
+ * File路径问题
+ * 用file类读取配置文件，虽然spring提供了resource接口以简化文件读取，但是研究这个还是有必要的
+ *
+ * @author 吴启欢
+ * @version 1.0
+ * @date 2018-12-27 20:48
+ */
+public class FilePathDemo {
+    public static void main(String[] args) {
+
+
+    }
+}
