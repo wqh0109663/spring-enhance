d29ba5bbfef43b046cb7b0a29b3357a09767b3ae
wqh0109663 <337096961@qq.com>
2018-12-27 20:52:26 +0800

    xml parser spring

--- src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/XmlParser.java
+++ src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/XmlParser.java
@@ -17,44 +17,41 @@ import java.io.File;
 public class XmlParser {
     public static void main(String[] args) throws Exception {
         try {
-            File inputFile = new File("C:\\Users\\Administrator\\Desktop\\aaa.txt");
+            String path = "src/main/java/top/shownmmp/spring/enhance/project/java/base/knowledge/xml/bean.xml";
+            System.out.println(path);
+            File file = new File(path);
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            System.out.println();
+            Document doc = dBuilder.parse(file);
             doc.getDocumentElement().normalize();
             System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("student");
+            NodeList list = doc.getElementsByTagName("bean");
             System.out.println("----------------------------");
 
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
+            for (int i = 0; i < list.getLength(); i++) {
+                Node nNode = list.item(i);
                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
 
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    System.out.println("Student roll no : "
-                            + eElement.getAttribute("rollno"));
-                    System.out.println("First Name : "
-                            + eElement
-                            .getElementsByTagName("firstname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Last Name : "
-                            + eElement
-                            .getElementsByTagName("lastname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Nick Name : "
-                            + eElement
-                            .getElementsByTagName("nickname")
-                            .item(0)
-                            .getTextContent());
-                    System.out.println("Marks : "
-                            + eElement
-                            .getElementsByTagName("marks")
-                            .item(0)
-                            .getTextContent());
+                    System.out.println("id--"+eElement.getAttribute("id"));
+
+                    System.out.println("class--"+eElement.getAttribute("class"));
+                    NodeList property = eElement.getElementsByTagName("property");
+                    for(int j = 0 ; j < property.getLength();j++){
+                        Node item = property.item(j);
+                        String nodeName = item.getNodeName();
+                        System.out.println("nodeName:"+nodeName);
+                        short nodeType = item.getNodeType();
+                        System.out.println("nodeType--"+nodeType);
+                        if (nodeType==Node.ELEMENT_NODE){
+                            Element name = (Element) item;
+                            System.out.println(" property name:---"+name.getAttribute("name"));
+                            System.out.println("property value:---"+name.getAttribute("value"));
+
+                        }
+                    }
+                    System.out.println("****");
                 }
             }
         } catch (Exception e) {
